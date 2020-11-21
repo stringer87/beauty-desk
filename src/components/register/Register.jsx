179fdeee26f4dcd7bdfacdb78a07e67f47e00845
stringer87 <mastringer87@gmail.com>
2020-11-21 14:46:27 -0800

    updated validation check for registration inputs

--- src/components/register/Register.jsx
+++ src/components/register/Register.jsx
@@ -2,7 +2,7 @@ import React, { useState, useContext } from 'react';
 import { DisplayContext } from '../context/displayContext.jsx';
 import axios from 'axios'
 
-const { RegisterWrapper, Input, Logo, Button, ButtonWrapper } = require('./registerStyles')
+const { RegisterWrapper, Input, Logo, Button, ButtonWrapper, ErrorMSGFirst, ErrorMSGLast, ErrorMSGEmail, ErrorMSGPass} = require('./registerStyles')
 function Register() {
   const [registerInfo, setRegisterInfo] = useState({
     firstName: '',
@@ -10,6 +10,50 @@ function Register() {
     email: '',
     password: ''
   })
+  const [errorMSGS, setErrorMSGS] = useState({
+    first: false,
+    last: false,
+    email: false,
+    pass:false
+  })
+
+  //validation for password
+  const passVall = (pass) => {
+    let res = false;
+    const reg = /^[a-zA-Z0-9!"#\$%&'\(\)\*\+,-\.\/:;<=>\?@[\]\^_`\{\|}~]{8,16}$/;
+    res = reg.test(pass);
+    setErrorMSGS((current) => ({...current, pass: !res}))
+    return res;
+  }
+  
+  //validation for email
+  const emailVal = (email) => {
+    const reg = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
+    let res = reg.test(email);
+    setErrorMSGS((current)=> ({...current, email: !res}))
+    return res;
+  }
+  
+  //validation for names
+  const nameVal = (name, pos) => {
+    const reg = /^[a-zA-Z]*$/;
+    let res = reg.test(name);
+    if(pos === 'first'){
+      if(name === ''){
+        setErrorMSGS((current) => ({...current, first: res}))
+      } else {
+        setErrorMSGS((current) => ({...current, first: !res}))
+      }
+    }
+    if(pos === 'last'){
+      if(name === ''){
+        setErrorMSGS((current) => ({...current, last: res}))
+      } else {
+        setErrorMSGS((current) => ({...current, last: !res}))
+      }
+    }
+    return res;
+  }
 
   //state for current display
   const [display, setDisplay] = useContext(DisplayContext);
@@ -25,13 +69,24 @@ function Register() {
   //handle button click events
   const handelClick = (e) => {
     let name = e.target.name;
+    let validIn = true;
     if (name === 'cancel') {
       setDisplay((current) => ({ ...current, login: true, register: false }))
     }
     if (name === 'submit') {
-      axios.post('http://localhost:5252/register', { params: registerInfo })
-        .then(({ data }) => { alert(emailVal(registerInfo.email)) })
-        .catch((err) => console.error(err))
+
+      if(!passVall(registerInfo.password) || !nameVal(registerInfo.lastName, 'last') || !nameVal(registerInfo.firstName, 'first') ||!emailVal(registerInfo.email)){
+        validIn = false;
+      }
+
+      if(validIn){
+        axios.post('http://localhost:5252/register', { params: registerInfo })
+          .then(({ data }) => { console.log('valid inputs recorded') })
+          .catch((err) => console.error(err))
+
+      } else{
+        alert('error')
+      }
     }
 
   }
@@ -40,9 +95,13 @@ function Register() {
     <RegisterWrapper>
       <Logo src="./src/img/BeautyDeskv2.png" onChange={handleRegisterUpdate} />
       <Input name="firstName" placeholder="First Name" onChange={handleRegisterUpdate} />
+{errorMSGS.first && <ErrorMSGFirst>Must be a vaild first name</ErrorMSGFirst>}
       <Input name="lastName" placeholder="Last Name" onChange={handleRegisterUpdate} />
+{ errorMSGS.last && <ErrorMSGLast>Must be a valid last name</ErrorMSGLast>}
       <Input name="email" placeholder="Email" onChange={handleRegisterUpdate} />
+{ errorMSGS.email &&<ErrorMSGEmail>Must be a valid email address</ErrorMSGEmail>}
       <Input name="password" placeholder="Password" type="password" onChange={handleRegisterUpdate} />
+{errorMSGS.pass &&<ErrorMSGPass>Must be a valid password</ErrorMSGPass>}
       <ButtonWrapper>
         <Button name="cancel" onClick={handelClick}>Cancel</Button>
         <Button name="submit" onClick={handelClick}>Submit</Button>
@@ -51,15 +110,5 @@ function Register() {
   </>
 }
 
-
-const emailVal = (email) => {
-  const reg = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
-  return reg.test(email);
-}
-
-const nameVal = (name) => {
-  const reg = /^[a-zA-Z]*$/;
-  return reg.test(name);
-}
 
 export default Register;
--- src/components/register/registerStyles.js
+++ src/components/register/registerStyles.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const RegisterWrapper = styled.div`
 width: 300px;
-height: 400px;
+height: 420px;
 margin: auto;
 position: absolute;
 top: 0;
@@ -20,7 +20,7 @@ justify-content: center;
 export const Input = styled.input`
 padding:8px;
 font-size: 18px;
-margin: 5px;
+margin: 9px;
 `;
 
 export const Logo = styled.img`
@@ -39,10 +39,32 @@ padding: 8px;
 min-width:90px;
 margin-left: 15px;
 margin-right: 15px;
-margin-top:5px;
-margin-bottom:5px;
+margin-top:7px;
+margin-bottom:7px;
 `;
 export const ButtonWrapper = styled.div`
 display:flex;
 justify-content: center;
+`;
+
+export const ErrorMSGFirst = styled.p`
+position: absolute;
+color: red;
+transform: translate(15px, -12px);
+`;
+
+export const ErrorMSGLast = styled.p`
+position: absolute;
+color: red;
+transform: translate(15px, 46px);
+`;
+export const ErrorMSGEmail = styled.p`
+position: absolute;
+color: red;
+transform: translate(15px, 105px);
+`;
+export const ErrorMSGPass = styled.p`
+position: absolute;
+color: red;
+transform: translate(15px, 163px);
 `;
