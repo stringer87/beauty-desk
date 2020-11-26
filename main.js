cb980554c54e1582d8517987f6863a2ec4e8a38a
Michael A Stringer <mastringer87@gmail.com>
2020-11-26 01:56:25 -0800

    cleaned up main.js

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 const { default: installExtension, REACT_DEVELOPER_TOOLS } = require('electron-devtools-installer');
 const { app, BrowserWindow } = require('electron')
 const path = require('path')
-const os = require('os')
 
 function createWindow() {
   const win = new BrowserWindow({
@@ -17,24 +16,11 @@ function createWindow() {
     }
   })
   win.loadFile('index.html')
-  win.addDevToolsExtension(
-    path.join(os.homedir(), '/Library/Application Support/Google/Chrome/Default/Extensions/fmkadmapgofadopljbjfkapdkoienihi/4.3.0_0')
-  ).then(() => {
-
-    win.webContents.openDevTools()
-  })
-    .catch((err) => {
-      console.error(err)
-    })
+  win.webContents.openDevTools()
 }
 
-// app.whenReady().then(createWindow)
-app.whenReady().then(() => {
-  installExtension(REACT_DEVELOPER_TOOLS)
-    .then((name) => console.log(`Added Extension:  ${name}`))
-    .catch((err) => console.log('An error occurred: ', err));
-  createWindow();
-});
+app.whenReady().then(createWindow)
+
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit()
